8a7357cedf457056da2c9fb424a6a6bc706fcc0e
saadiqks <saadiqks@gmail.com>
2020-03-08 17:41:53 -0400

    Fixed baseline code.

--- cqi_cpp/src/wrapper/cqi_test.py
+++ cqi_cpp/src/wrapper/cqi_test.py
@@ -1,4 +1,6 @@
+import argparse 
 import gym
+import math
 
 from qtree_wrapper import PyBox as Box
 from qtree_wrapper import PyDiscrete as Discrete
@@ -9,18 +11,49 @@ from train import Train
 
 from utils import convert_to_pybox
 
-env = gym.make('MountainCar-v0')
+env = gym.make('CartPole-v0')
+
+def truncate(number, digits):
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
 
 box = convert_to_pybox(env.observation_space)
 
 discrete = Discrete(env.action_space.n)
-qfunc = QTree(box, discrete, None, gamma=0.99, alpha=0.01, visit_decay=0.999, \
-        split_thresh_max=0.1, split_thresh_decay=0.99, num_splits=2)
+
+# Optional command line args
+parser = argparse.ArgumentParser()
+parser.add_argument("--gamma") 
+parser.add_argument("--alpha")
+parser.add_argument("--visit_decay")
+parser.add_argument("--split_thresh_max")
+parser.add_argument("--split_thresh_decay")
+parser.add_argument("--num_splits")
+parser.add_argument("--grid_search")
+parser.add_argument("--steps")
+args = parser.parse_args()
+
+gamma = float(args.gamma) if args.gamma else 0.99
+alpha = float(args.alpha) if args.alpha else 0.01
+visit_decay = float(args.visit_decay) if args.visit_decay else 0.999
+split_thresh_max = float(args.split_thresh_max) if args.split_thresh_max else 0.1
+split_thresh_decay = float(args.split_thresh_decay) if args.split_thresh_decay else 0.99
+num_splits = int(args.num_splits) if args.num_splits else 2
+grid_search = bool(args.grid_search) if args.grid_search else False
+
+qfunc = QTree(box, discrete, None,
+    # Hyperparameters
+    gamma,
+    alpha,
+    visit_decay,
+    split_thresh_max,
+    split_thresh_decay,
+    num_splits)
 
 t = Train(qfunc, env)
 
 eps_func = (lambda step: max(0.05, 1 - step/1e5))
-train_steps = int(5e3)
+train_steps = int(args.steps) if args.steps else int(5e5)
 
 # Training
 history = t.train(train_steps, eps_func, verbose=True, qfunc_hist=None)
@@ -30,7 +63,18 @@ results, history2, avg_r_per_ep, _ = t.train(50000, lambda step: 0.05, \
         verbose=True, eval_only=True, penalty_check=lambda s, r: r <= -1000, \
         track_data_per=1, run_tag="some descriptive tag for logging")
 
-# qfunc.print_structure()
-print(f"Number of nodes: {qfunc.num_nodes()}") 
-print(f"Average reward per episode: {avg_r_per_ep}")
+qfunc.print_structure()
+nodes = f"\nNumber of nodes: {qfunc.num_nodes()}\n" 
+reward = f"\nAverage reward per episode: {truncate(avg_r_per_ep, 3)}\n"
+hparams_str = f"gamma={gamma}, alpha={alpha}, visit_decay={visit_decay}, " 
+hparams_str += f"split_thresh_max={split_thresh_max}, " 
+hparams_str += f"split_thresh_decay={split_thresh_decay}, num_splits={num_splits}"
+
+if grid_search:
+    with open("grid_search_results.txt", "a") as myfile:
+        myfile.write(nodes + reward + hparams_str)
+
+    myfile.close()
+else:
+    print(nodes + reward + hparams_str)
 
--- cqi_cpp/src/wrapper/example.py
+++ cqi_cpp/src/wrapper/example.py
@@ -1,21 +1,49 @@
+import argparse 
 import gym
+import math
 
 from qtree import QTree
 from py_train import Train
 
-env = gym.make('MountainCar-v0')
+env = gym.make('LunarLander-v2')
 
-qfunc = QTree(env.observation_space, env.action_space,
+def truncate(number, digits):
+    stepper = 10.0 ** digits
+    return math.trunc(stepper * number) / stepper
+
+# Optional command line args
+parser = argparse.ArgumentParser()
+parser.add_argument("--gamma") 
+parser.add_argument("--alpha")
+parser.add_argument("--visit_decay")
+parser.add_argument("--split_thresh_max")
+parser.add_argument("--split_thresh_decay")
+parser.add_argument("--num_splits")
+parser.add_argument("--grid_search")
+parser.add_argument("--steps")
+args = parser.parse_args()
+
+gamma = float(args.gamma) if args.gamma else 0.99
+alpha = float(args.alpha) if args.alpha else 0.01
+visit_decay = float(args.visit_decay) if args.visit_decay else 0.999
+split_thresh_max = float(args.split_thresh_max) if args.split_thresh_max else 0.1
+split_thresh_decay = float(args.split_thresh_decay) if args.split_thresh_decay else 0.99
+num_splits = int(args.num_splits) if args.num_splits else 2
+grid_search = bool(args.grid_search) if args.grid_search else False
+
+qfunc = QTree(env.observation_space, env.action_space, None,
                   # Hyperparameters
-                  gamma=0.99,
-                  alpha=0.01,
-                  visit_decay=0.999,
-                  split_thresh_max=0.1,
-                  split_thresh_decay=0.99)
+                  gamma,
+                  alpha,
+                  visit_decay,
+                  split_thresh_max,
+                  split_thresh_decay,
+                  num_splits)
 t = Train(qfunc, env)
 
 eps_func = (lambda step: max(0.05, 1 - step/1e5))
-train_steps = int(5e3)
+
+train_steps = int(args.steps) if args.steps else int(5e5)
 
 # normal execution:
 # Training
@@ -25,6 +53,17 @@ history = t.train(train_steps, eps_func, verbose=True, qfunc_hist=None)
 results, history2, avg_r_per_ep, _ = t.train(50000, lambda step: 0.05, verbose=True, eval_only=True, penalty_check=lambda s, r: r <= -1000, track_data_per=1, run_tag="some descriptive tag for logging")
 
 # qfunc.print_structure()
-nn = qfunc.num_nodes()
-print("Number of nodes: %d, %d" % (nn, qfunc.num_nodes()))
-print("Average reward per episode: %f" % avg_r_per_ep)
+nodes = f"\nNumber of nodes: {qfunc.num_nodes()}\n" 
+reward = f"\nAverage reward per episode: {truncate(avg_r_per_ep, 3)}\n"
+hparams_str = f"gamma={gamma}, alpha={alpha}, visit_decay={visit_decay}," 
+hparams_str += f"split_thresh_max={split_thresh_max}," 
+hparams_str += f"split_thresh_decay={split_thresh_decay}, num_splits={num_splits}"
+
+if grid_search:
+    with open("grid_search_results.txt", "a") as myfile:
+        myfile.write(nodes + reward + hparams_str)
+
+    myfile.close()
+else:
+    print(nodes + reward + hparams_str)
+
