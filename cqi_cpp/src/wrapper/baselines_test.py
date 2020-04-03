0297ccd490bc6e8dc7904edd24e259ae3ee5e261
saadiqks <saadiqks@gmail.com>
2020-04-03 12:08:45 -0400

    Added PPO files.

--- cqi_cpp/src/wrapper/baselines_test.py
+++ cqi_cpp/src/wrapper/baselines_test.py
@@ -6,7 +6,7 @@ import numpy as np
 
 # TODO: add more flags for environment and hparams, rewrite grid_search.sh 
 
-def evaluate(model, num_steps=1000):
+def evaluate(model, env, num_steps=1000):
   """
   Evaluate a RL agent
   :param model: (BaseRLModel object) the RL Agent
@@ -29,37 +29,57 @@ def evaluate(model, num_steps=1000):
   # Compute mean reward for the last 100 episodes
   mean_100ep_reward = round(np.mean(episode_rewards[-100:]), 1)
   
-  print(f"Mean reward: {mean_100ep_reward}, Num episodes: {len(episode_rewards)}")
+  # print(f"Mean reward: {mean_100ep_reward}, Num episodes: {len(episode_rewards)}")
   
   return mean_100ep_reward
 
-parser = argparse.ArgumentParser()
-parser.add_argument("--algorithm") 
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--algorithm") 
+    parser.add_argument("--env")
+    parser.add_argument("--steps")
+    parser.add_argument("--alpha")
+    parser.add_argument("--grid_search")
+    args = parser.parse_args()
 
-algorithm = args.algorithm 
+    algorithm = args.algorithm 
+    env = gym.make(args.env)
+    grid_search = args.grid_search
+    alpha = args.alpha
 
-env = gym.make('CartPole-v0')
+    if algorithm == "ppo1":
+        from stable_baselines import PPO1
+        from stable_baselines.common.policies import MlpPolicy
+        
+        model = PPO1(MlpPolicy, env, verbose=1)
+    else:
+        from stable_baselines import DQN
+        from stable_baselines.deepq.policies import MlpPolicy
 
-if algorithm == "ppo1":
-    from stable_baselines import PPO1
-    from stable_baselines.common.policies import MlpPolicy
+        model = DQN(MlpPolicy, env, learning_rate=alpha, verbose=1)
+
+    model.learn(total_timesteps=int(args.steps), log_interval=10)
+    model.save(f"{algorithm}_cartpole")
+
+    del model # remove to demonstrate saving and loading
+
+    if algorithm == "ppo1":
+        model = PPO1.load(f"{algorithm}_cartpole")
+    else:
+        model = DQN.load(f"{algorithm}_cartpole")
+
+    mean_reward = evaluate(model, env, num_steps=10000)
     
-    model = PPO1(MlpPolicy, env, verbose=1)
-else:
-    from stable_baselines import DQN
-    from stable_baselines.deepq.policies import MlpPolicy
+    hparams_str = f" algorithm={algorithm} env={args.env} steps={args.steps} alpha={alpha}"
 
-    model = DQN(MlpPolicy, env, verbose=1)
+    if grid_search:
+        with open("grid_search_results.txt", "a") as myfile:
+            myfile.write(str(mean_reward) + hparams_str)
 
-model.learn(total_timesteps=int(2e4), log_interval=10)
-model.save(f"{algorithm}_cartpole")
+        myfile.close()
+    else:
+        print(str(mean_reward) + hparams_str)
 
-del model # remove to demonstrate saving and loading
+if __name__ == '__main__': 
+    main()
 
-if algorithm == "ppo1":
-    model = PPO1.load(f"{algorithm}_cartpole")
-else:
-    model = DQN.load(f"{algorithm}_cartpole")
-
-mean_reward = evaluate(model, num_steps=10000)
